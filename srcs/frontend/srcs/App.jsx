e03627004bb1281195fe7cf9a5e9e700f892d56f
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-05-20 12:09:40 +0200

    feat(footer): add clickable logo for github and clickable text for terms and policies

--- srcs/frontend/srcs/App.jsx
+++ srcs/frontend/srcs/App.jsx
@@ -8,6 +8,8 @@ import Login from './pages/Login/Login.jsx';
 import Register from './pages/Register/Register.jsx';
 import Upload from './pages/Upload/Upload.jsx';
 import UserSession from './pages/UserSession/UserSession.jsx';
+import PrivacyPolicy from './pages/TermsAndPolicies/PrivacyPolicy.jsx'
+import TermsOfService from './pages/TermsAndPolicies/TermsOfService.jsx'
 
 function App() {
 	return (
@@ -17,12 +19,13 @@ function App() {
       <Route path="/backend" element={<Test_back_end />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-	  <Route path="/upload" element={<Upload />} />
-	  <Route path="/session" element={<UserSession />} />
+	    <Route path="/upload" element={<Upload />} />
+	    <Route path="/session" element={<UserSession />} />
+      <Route path="/privacy_policy" element={<PrivacyPolicy />} />
+      <Route path="/terms_of_service" element={<TermsOfService />} />
     </Routes>
   </BrowserRouter>
 	)
 }
-
 
 export default App;
